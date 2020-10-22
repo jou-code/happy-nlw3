17f28260cd97c89f8a19b7eafa648646cdff9c82
jou-code <70237361+jou-code@users.noreply.github.com>
2020-10-22 18:07:02 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+//iniciando o express
+const express = require('express');
+const path = require('path');
+const pages = require('./pages.js');
+
+//iniciando o express
+const server = express()
+server
+    //utilizando os arquivos estáticos
+    .use(express.static('public'))
+
+    //config template engine
+    .set('views', path.join(__dirname, 'src/views'))
+    .set('view engine', 'hbs')
+
+    //rotas da aplicação
+    .get('/', pages.index)
+    .get('/orphanage', pages.orphanage)
+    .get('/orphanages', pages.orphanages)
+    .get('/create-orphanage', pages.createOrphanage)
+        
+
+
+//ligar o servidor
+server.listen(5500)
